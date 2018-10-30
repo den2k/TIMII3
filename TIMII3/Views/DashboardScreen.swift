737cc1ccf6ba83c50eece3056e7066f76e641b3a
Dennis Huang <den2k@ymail.com>
2018-10-29 18:52:48 -0700

    Added tab bar icons to Assets folder. Also added new 'blank' Dashboard Screen to app.

---
+++ TIMII3/Views/DashboardScreen.swift
@@ -0,0 +1,15 @@
+//
+//  DashboardScreen.swift
+//  TIMII3
+//
+//  Created by Dennis Huang on 10/29/18.
+//  Copyright © 2018 Autonomii. All rights reserved.
+//
+
+import Foundation
+import Layout
+
+class DashboardScreen: UIViewController
+{
+    @IBOutlet var DashboardScreenNode: LayoutNode?
+}
